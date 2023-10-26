7e9e27249e4fca4e464e893cbb9f35b345f65d5a
ElifKumbetli <elifkumbetli@gmail.com>
2023-10-26 23:47:02 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstact;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+
+    //Çıplak Class Kalmasın
+
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+
+        public int CategoryName { get; set; }
+    }
+}
