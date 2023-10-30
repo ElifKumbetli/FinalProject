3cdab3a72eb4954d2fef5d16c0dd9e29a60aebcc
ElifKumbetli <elifkumbetli@gmail.com>
2023-10-30 23:52:06 +0300

    30 Ekim

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -11,5 +11,13 @@ namespace DataAccess.Abstract
     {
         List<Product> GetAll();
 
+        void Add(Product product);
+
+        void Update(Product product);
+
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId);
+
     }
 }
